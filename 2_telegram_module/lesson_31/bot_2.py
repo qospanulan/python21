58e2fe2400a7437f55c5140a281650a3f2f585e5
qospanulan <kospanovulan@gmail.com>
2024-10-10 19:39:25 +0500

    lesson 31. Telegram Bot. 1.

--- 2_telegram_module/lesson_31/bot_2.py
+++ 2_telegram_module/lesson_31/bot_2.py
@@ -1,5 +1,20 @@
+import telebot
+
+TOKEN = '7800185758:AAEkd4LIRIDvO53CKy4Orueht8SyUJNgxBo'
+
+bot = telebot.TeleBot(token=TOKEN)
 
 
+@bot.message_handler(content_types=['text'])
+def repeat_all_messages(msg):
+    print("принял какое то сообщение")
+    # print(type(msg))
+    # print(msg)
 
-token = '7800185758:AAEkd4LIRIDvO53CKy4Orueht8SyUJNgxBo'
+    print(msg.text)
+    bot.send_message(chat_id=msg.chat.id, text=msg.text)
+
+
+bot.infinity_polling()
+
 
